7ecbd6f169166c1d2c3299f5f551b4ec171bd91b
JoaoRicardoBannachCasara <jrbcasara@gmail.com>
2025-05-30 18:17:11 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function saudar(nome) {
-    return `Olá, ${nome}! Bem-vindo(a) ao TypeScript.`;
-}
-let usuario = "Estudante";
-console.log(saudar(usuario));
-// Tente introduzir um erro de tipo para ver o VS Code e o compilador agindo:
-// usuario = 123; // Isso mostrará um erro no VS Code
